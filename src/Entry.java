29c58a1260e6dd34214f653311d5771676abc5d1
aadesh.agarwal8888 <aadesh.agarwal8888@gmail.com>
2019-10-09 19:58:54 +0530

    first commit

--- src/Entry.java
+++ src/Entry.java
@@ -2,13 +2,23 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class Entry {
 
+	
+	static Connection connection=null;
+	static PreparedStatement stSelect=null;
+	static ResultSet result=null;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		boolean start = true;
 		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection=DriverManager.getConnection("jdbc:mysql://localhost/sys","root","root");
 			ServerSocket serverSocket = new ServerSocket(9000);
 			while(start) {
 				System.out.println("Waiting for Client");
@@ -16,7 +26,8 @@ public class Entry {
 				Socket client = serverSocket.accept(); //This statement waits for client
 				
 				System.out.println("Client Connected: " + client.getRemoteSocketAddress());
-				
+				stSelect=connection.prepareStatement("select * from emp;");
+				result = stSelect.executeQuery();
 				String data[] = {"Amul","Chitale","Thorat","Mitkari"};
 				
 				DataOutputStream dos = new DataOutputStream(client.getOutputStream());	//Sending Data
